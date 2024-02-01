52c7e59a143bb7fc2f72a223dcdad8e79e7aaa29
johnlopez0505 <lopezcon1@hotmail.com>
2024-02-01 14:43:45 +0100

    version 0.0.2

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
 
   const [juegos, setJuego]=useState([]);
   const FindBooks = async() => {
-    const response = await fetch(" http://localhost:3500/results");
+    const response = await fetch(" http://localhost:3500/videojuegos");
     const videojuego = await response.json();
-    setJuego(videojuego[0]);
-    console.log(videojuego[0]);
+    setJuego(videojuego);
+    console.log(videojuego);
   
   }
 
